a77e42c43d13f12657ac2f5cd790b983810a3de2
jvictormleao <joaovictormarianoleao@gmail.com>
2022-12-14 22:14:10 -0300

    Initial commit

---
+++ public/fonts/liga.js
@@ -0,0 +1,76 @@
+/* A polyfill for browsers that don't support ligatures. */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* To provide support for elements dynamically added, this script adds
+   method 'icomoonLiga' to the window object. You can pass element references to this method.
+*/
+(function () {
+    'use strict';
+    function supportsProperty(p) {
+        var prefixes = ['Webkit', 'Moz', 'O', 'ms'],
+            i,
+            div = document.createElement('div'),
+            ret = p in div.style;
+        if (!ret) {
+            p = p.charAt(0).toUpperCase() + p.substr(1);
+            for (i = 0; i < prefixes.length; i += 1) {
+                ret = prefixes[i] + p in div.style;
+                if (ret) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    var icons;
+    if (!supportsProperty('fontFeatureSettings')) {
+        icons = {
+            'login': '&#xe900;',
+            'key': '&#xe901;',
+            'archive': '&#xe902;',
+            'icon-add': '&#xe903;',
+            'credit-card': '&#xe93f;',
+            'money5': '&#xe93f;',
+            'eye': '&#xe9ce;',
+            'views': '&#xe9ce;',
+            'eye-blocked': '&#xe9d1;',
+            'views4': '&#xe9d1;',
+            'arrow-right2': '&#xea3c;',
+            'right4': '&#xea3c;',
+            'arrow-left2': '&#xea40;',
+            'left4': '&#xea40;',
+          '0': 0
+        };
+        delete icons['0'];
+        window.icomoonLiga = function (els) {
+            var classes,
+                el,
+                i,
+                innerHTML,
+                key;
+            els = els || document.getElementsByTagName('*');
+            if (!els.length) {
+                els = [els];
+            }
+            for (i = 0; ; i += 1) {
+                el = els[i];
+                if (!el) {
+                    break;
+                }
+                classes = el.className;
+                if (/icon-/.test(classes)) {
+                    innerHTML = el.innerHTML;
+                    if (innerHTML && innerHTML.length > 1) {
+                        for (key in icons) {
+                            if (icons.hasOwnProperty(key)) {
+                                innerHTML = innerHTML.replace(new RegExp(key, 'g'), icons[key]);
+                            }
+                        }
+                        el.innerHTML = innerHTML;
+                    }
+                }
+            }
+        };
+        window.icomoonLiga();
+    }
+}());
